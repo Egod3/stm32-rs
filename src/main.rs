26e63fb35a69a6b40280f492d0d5f0515e7373a4
Ezra Godfrey <ezra.godfrey@gmail.com>
2022-02-12 10:40:05 -0700

    Trying to enbale the STM32L476RG chip
    Attempting to get gdb/qemu/openocd to all work with the above chip.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 // pick a panicking behavior
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+
 // use panic_abort as _; // requires nightly
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
@@ -23,6 +24,5 @@ fn main() -> ! {
     // NOTE do not run this on hardware; it can corrupt OpenOCD state
     debug::exit(debug::EXIT_SUCCESS);
 
-    loop {
-    }
+    loop {}
 }
