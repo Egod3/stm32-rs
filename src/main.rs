f8ddcfe1634e531449ec67c208d6e37cba0d5d16
Ezra J Godfrey <ezra.godfrey@gmail.com>
2024-01-21 10:00:35 -0700

    add UART init and hello world print
    Limited scope of unsafe code to the steal call.
    Disabled the timer and sleep loop since the semi-hosting
    prints are delay enough to see the LED flashing.

--- src/main.rs
+++ src/main.rs
@@ -3,81 +3,118 @@
 
 use cortex_m::delay::Delay;
 use cortex_m::iprintln;
+use cortex_m::prelude::_embedded_hal_blocking_serial_Write;
 use cortex_m_rt::entry;
 use panic_halt as _;
 use stm32l4xx_hal::gpio::GpioExt;
 use stm32l4xx_hal::gpio::*;
 use stm32l4xx_hal::rcc::RccExt;
+use stm32l4xx_hal::serial::Rx;
+use stm32l4xx_hal::serial::Tx;
 use stm32l4xx_hal::stm32::Peripherals;
+
+//const _BAUD: u32 = 460_800;
+const _BAUD: u32 = 230_400;
 
 #[entry]
 fn main() -> ! {
     let mut i2c1 = si70xx::i2c_init();
 
+    let mut p;
     unsafe {
-        let mut p = cortex_m::Peripherals::steal();
-        let stim = &mut p.ITM.stim[0];
-        if !stim.is_fifo_ready() {
-            //iprintln!(stim, "stim.is_fifo_ready() returned false...\n");
-        }
-        while !stim.is_fifo_ready() {}
+        p = cortex_m::Peripherals::steal();
+    }
+    let stim = &mut p.ITM.stim[0];
+    if !stim.is_fifo_ready() {
+        //iprintln!(stim, "stim.is_fifo_ready() returned false...\n");
+    }
+    while !stim.is_fifo_ready() {}
 
+    iprintln!(
+        stim,
+        "stm32-rs application which displays relative temp/humidity, via ITM\n"
+    );
+
+    // Initialize and test the UART
+    let (_tx, _rx) = test_uart(stim, _BAUD);
+
+    // Initialize the LED
+    let mut led = led_init();
+
+    let fw_ver = si70xx::get_fw_version(&mut i2c1);
+    si70xx::iprint_fw_version(stim, fw_ver);
+    let sensor_id = si70xx::get_sensor_id(&mut i2c1);
+    si70xx::iprint_sensor_id(stim, sensor_id);
+
+    // TODO: find out how to convert from Hertz to u32 here... so we can use clocks.hclk() in place
+    // of 16_000_000
+    //let _cp;
+    //unsafe {
+    //    _cp = cortex_m::Peripherals::steal();
+    //}
+    //let mut _timer = Delay::new(_cp.SYST, 16_000_000);
+    led.set_high();
+
+    let mut led_state: bool = false;
+    loop {
+        let temperature = si70xx::get_rel_temperature(&mut i2c1);
+        let humidity = si70xx::get_rel_humidity(&mut i2c1);
+        si70xx::iprint_temperature(stim, temperature);
+        si70xx::iprint_humidity(stim, humidity);
+        si70xx::hprint_temperature(temperature);
+        si70xx::hprint_humidity(humidity);
+
+        //_timer.delay_ms(1000_u32);
+        if led_state {
+            toggle_led(stim, &mut led, PinState::High);
+            led_state = false;
+        } else {
+            toggle_led(stim, &mut led, PinState::Low);
+            led_state = true;
+        }
         iprintln!(
             stim,
             "stm32-rs application which displays relative temp/humidity, via ITM\n"
         );
-
-        // initialize the LED
-        let mut led = led_init();
-
-        let fw_ver = si70xx::get_fw_version(&mut i2c1);
-        si70xx::iprint_fw_version(stim, fw_ver);
-        let sensor_id = si70xx::get_sensor_id(&mut i2c1);
-        si70xx::iprint_sensor_id(stim, sensor_id);
-
-        // TODO: find out how to convert from Hertz to u32 here... so we can use clocks.hclk() in place
-        // of 16_000_000
-        let cp = cortex_m::Peripherals::steal();
-        let mut _timer = Delay::new(cp.SYST, 16_000_000);
-        led.set_high();
-
-        let mut led_state: bool = false;
-        loop {
-            let temperature = si70xx::get_rel_temperature(&mut i2c1);
-            let humidity = si70xx::get_rel_humidity(&mut i2c1);
-            si70xx::iprint_temperature(stim, temperature);
-            si70xx::iprint_humidity(stim, humidity);
-            si70xx::hprint_temperature(temperature);
-            si70xx::hprint_humidity(humidity);
-
-            _timer.delay_ms(1000_u32);
-            if led_state {
-                toggle_led(stim, &mut led, PinState::High);
-                led_state = false;
-            } else {
-                toggle_led(stim, &mut led, PinState::Low);
-                led_state = true;
-            }
-            iprintln!(
-                stim,
-                "stm32-rs application which displays relative temp/humidity, via ITM\n"
-            );
-        }
     }
+}
+
+fn test_uart(
+    _stim: &mut cortex_m::peripheral::itm::Stim,
+    _baud: u32,
+) -> (
+    Tx<stm32l4xx_hal::pac::USART2>,
+    Rx<stm32l4xx_hal::pac::USART2>,
+) {
+    let (mut tx, _rx) = uart_stm32::uart_init(_stim, _BAUD);
+
+    iprintln!(_stim, "uart_tx_test, logging via ITM\n");
+
+    let buffer = b"\n\rHello World, over UART\n\r";
+    let _result = tx.bwrite_all(buffer);
+    iprintln!(
+        _stim,
+        "Check the uart, running at {} baud for the blow string:\n",
+        _BAUD
+    );
+    iprintln!(_stim, "{:?}\n", buffer);
+
+    (tx, _rx)
 }
 
 fn led_init() -> stm32l4xx_hal::gpio::PA5<stm32l4xx_hal::gpio::Output<stm32l4xx_hal::gpio::PushPull>>
 {
+    let dp: stm32l4xx_hal::stm32::Peripherals;
     unsafe {
         // TODO: Is there a safe way to take the dp pac::Peripherals::take().unwrap()  ?!?
-        let dp = Peripherals::steal();
-        let mut rcc = dp.RCC.constrain();
-        let mut gpioa = dp.GPIOA.split(&mut rcc.ahb2);
-
-        gpioa
-            .pa5
-            .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper)
+        dp = Peripherals::steal();
     }
+    let mut rcc = dp.RCC.constrain();
+    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb2);
+
+    gpioa
+        .pa5
+        .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper)
 }
 
 fn toggle_led(
